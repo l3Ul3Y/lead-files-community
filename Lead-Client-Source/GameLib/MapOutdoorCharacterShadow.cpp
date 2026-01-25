01547876846d9b611c87cf20b95feeaea4915b75
Lead0x194 <not_found@lead.com>
2026-01-25 21:47:51 +0100

    fix: re-add checks in MapOutdoorCharaxterShadow.cpp

--- Lead-Client-Source/GameLib/MapOutdoorCharacterShadow.cpp
+++ Lead-Client-Source/GameLib/MapOutdoorCharacterShadow.cpp
@@ -89,24 +89,52 @@ bool CMapOutdoor::BeginRenderCharacterShadowToTexture()
 	STATEMANAGER.SetTextureStageState(0, D3DTSS_ALPHAOP, D3DTOP_DISABLE);
 	STATEMANAGER.SetTextureStageState(1, D3DTSS_COLOROP, D3DTOP_DISABLE);
 
-	ms_lpd3dDevice->GetDepthStencilSurface(&m_lpBackupDepthSurface);
-	ms_lpd3dDevice->GetRenderTarget(0, &m_lpBackupRenderTargetSurface);
+	bool bSuccess = true;
 
-	ms_lpd3dDevice->SetRenderTarget(0, m_lpCharacterShadowMapRenderTargetSurface);
-	ms_lpd3dDevice->SetDepthStencilSurface(m_lpCharacterShadowMapDepthSurface);
+	// Backup Device Context
+	if (FAILED(ms_lpd3dDevice->GetRenderTarget(0, &m_lpBackupRenderTargetSurface)))
+	{
+		TraceError("CMapOutdoor::BeginRenderCharacterShadowToTexture : Unable to Save Window Render Target\n");
+		bSuccess = false;
+	}
 
-	ms_lpd3dDevice->Clear(
-		0, NULL,
-		D3DCLEAR_TARGET | D3DCLEAR_ZBUFFER,
-		0xFFFFFFFF,
-		1.0f,
-		0
-	);
+	if (FAILED(ms_lpd3dDevice->GetDepthStencilSurface(&m_lpBackupDepthSurface)))
+	{
+		TraceError("CMapOutdoor::BeginRenderCharacterShadowToTexture : Unable to Save Window Depth Surface\n");
+		bSuccess = false;
+	}
 
-	ms_lpd3dDevice->GetViewport(&m_BackupViewport);
-	ms_lpd3dDevice->SetViewport(&m_ShadowMapViewport);
+	if (FAILED(ms_lpd3dDevice->SetRenderTarget(0, m_lpCharacterShadowMapRenderTargetSurface)))
+	{
+		TraceError("CMapOutdoor::BeginRenderCharacterShadowToTexture : Unable to Set Shadow Map Render Target\n");
+		bSuccess = false;
+	}
 
-	return true;
+	if (FAILED(ms_lpd3dDevice->SetDepthStencilSurface(m_lpCharacterShadowMapDepthSurface)))
+	{
+		TraceError("CMapOutdoor::BeginRenderCharacterShadowToTexture : Unable to Set Shadow Map Render Target\n");
+		bSuccess = false;
+	}
+
+	if (FAILED(ms_lpd3dDevice->Clear(0L, NULL, D3DCLEAR_TARGET | D3DCLEAR_ZBUFFER, D3DCOLOR_XRGB(0xFF, 0xFF, 0xFF), 1.0f, 0)))
+	{
+		TraceError("CMapOutdoor::BeginRenderCharacterShadowToTexture : Unable to Clear Render Target");
+		bSuccess = false;
+	}
+
+	if (FAILED(ms_lpd3dDevice->GetViewport(&m_BackupViewport)))
+	{
+		TraceError("CMapOutdoor::BeginRenderCharacterShadowToTexture : Unable to Save Window Viewport\n");
+		bSuccess = false;
+	}
+
+	if (FAILED(ms_lpd3dDevice->SetViewport(&m_ShadowMapViewport)))
+	{
+		TraceError("CMapOutdoor::BeginRenderCharacterShadowToTexture : Unable to Set Shadow Map viewport\n");
+		bSuccess = false;
+	}
+
+	return bSuccess;
 }
 
 void CMapOutdoor::EndRenderCharacterShadowToTexture()
