2fe3def8092ae1f96110145a5a487d5ceef7870a
Dex <root@localhost>
2026-01-28 19:33:06 +0100

    Silence is beautiful and generalizing scripts for making  links too.

--- Lead-Serverfiles/create_links.bat
+++ Lead-Serverfiles/create_links.bat
@@ -1,12 +1,10 @@
 @echo off
-
 for %%x in (db, auth, channel1/game1, channel1/game2, channel99) do (
+	@rmdir /q "%~dp0%%x\data" >nul 2>&1
+	@del /Q "%~dp0%%x\data" >nul 2>&1
+	@mklink /D "%~dp0%%x\data" "%~dp0share\data"
 
-	@rmdir /q "C:/Lead-Git/Lead-Serverfiles/%%x/data" >nul 2>&1
-	@del /Q "C:/Lead-Git/Lead-Serverfiles/%%x\data" >nul 2>&1
-	@mklink /D "C:/Lead-Git/Lead-Serverfiles/%%x/data" "C:/Lead-Git/Lead-Serverfiles/share/data"
-
-	rmdir /q "C:/Lead-Git/Lead-Serverfiles/%%x/locale" >nul 2>&1
-	@del /Q "C:/Lead-Git/Lead-Serverfiles/%%x\locale" >nul 2>&1
-	@mklink /D "C:/Lead-Git/Lead-Serverfiles/%%x/locale" "C:/Lead-Git/Lead-Serverfiles/share/locale"
+	@rmdir /q "%~dp0%%x\locale" >nul 2>&1
+	@del /Q "%~dp0%%x\locale" >nul 2>&1
+	@mklink /D "%~dp0%%x\locale" "%~dp0share\locale"
 )
